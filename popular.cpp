fb0d10496dcff6f7f50d9880811726d761a1f22b
Jaymeen Devatka <147510153+JaymeenDevatka@users.noreply.github.com>
2024-11-28 13:47:21 +0530

    main

--- popular.cpp
+++ popular.cpp
@@ -2,12 +2,15 @@
 #include <vector>
 using namespace std;
 
-int SpecialInteger(vector<int>& array) {
+int SpecialInteger(vector<int> &array)
+{
     int n = array.size();
     int threshold = n / 4;
 
-    for (int i = 0; i < n; i++) {
-        if (array[i] == array[i + threshold]) {
+    for (int i = 0; i < n; i++)
+    {
+        if (array[i] == array[i + threshold])
+        {
             return array[i];
         }
     }
@@ -15,17 +18,19 @@ int SpecialInteger(vector<int>& array) {
     return -1; // No special integer found (though there should be one as per problem statement)
 }
 
-int main() {
+int main()
+{
     int n;
     cin >> n;
     vector<int> array(n);
-    
-    for (int i = 0; i < n; i++) {
+
+    for (int i = 0; i < n; i++)
+    {
         cin >> array[i];
     }
 
     int result = SpecialInteger(array);
-    
+
     cout << "Special Integer: " << result << endl;
 
     return 0;
